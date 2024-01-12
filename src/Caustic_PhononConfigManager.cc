3b7e28e248288785c6e75258b45db354f0c26ce7
Israel <96498069+Israel-Tanjiro@users.noreply.github.com>
2024-01-12 16:12:38 -0600

    Update Caustic_PhononConfigManager.cc

--- src/Caustic_PhononConfigManager.cc
+++ src/Caustic_PhononConfigManager.cc
@@ -23,10 +23,6 @@ Caustic_PhononConfigManager* Caustic_PhononConfigManager::Instance() {
 Caustic_PhononConfigManager::Caustic_PhononConfigManager()
   : Hit_file(getenv("G4CMP_HIT_FILE")?getenv("G4CMP_HIT_FILE"):"phonon_hits.txt"),
     messenger(new Caustic_PhononConfigMessenger(this))
-    ,Millerh(getenv("G4CMP_Millerh")?strtod(getenv("G4CMP_Millerh"),0):1.0)// New commands to support 4 Miller indices
-    ,Millerk(getenv("G4CMP_Millerk")?strtod(getenv("G4CMP_Millerk"),0):1.0)
-    ,Milleri(getenv("G4CMP_Milleri")?strtod(getenv("G4CMP_Milleri"),0):1.0)
-    ,Millerl(getenv("G4CMP_Millerl")?strtod(getenv("G4CMP_Millerl"),0):1.0)
 {;}
 
 Caustic_PhononConfigManager::~Caustic_PhononConfigManager() {
