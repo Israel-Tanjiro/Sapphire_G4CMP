2c3efe1bb165771703f1afceec3db26b894986bc
Israel <96498069+Israel-Tanjiro@users.noreply.github.com>
2024-01-12 16:13:00 -0600

    Update Caustic_PhononConfigMessenger.cc

--- src/Caustic_PhononConfigMessenger.cc
+++ src/Caustic_PhononConfigMessenger.cc
@@ -17,19 +17,13 @@ Caustic_PhononConfigMessenger::Caustic_PhononConfigMessenger(Caustic_PhononConfi
     theManager(mgr), hitsCmd(0) {
   hitsCmd = CreateCommand<G4UIcmdWithAString>("HitsFile",
 			      "Set filename for output of phonon hit locations");
-  MillerhCmd = CreateCommand<G4UIcmdWithADouble>("Millerh","Set the 1 Miller Indice");// New commands to support 4 Miller indice
- MillerkCmd = CreateCommand<G4UIcmdWithADouble>("Millerk","Set the 2 Miller Indice");
-MilleriCmd = CreateCommand<G4UIcmdWithADouble>("Milleri","Set the 3 Miller Indice");
-MillerlCmd = CreateCommand<G4UIcmdWithADouble>("Millerl","Set the 4 Miller Indice");
+
 }
 
 
 Caustic_PhononConfigMessenger::~Caustic_PhononConfigMessenger() {
   delete hitsCmd; hitsCmd=0;
-  delete MillerhCmd; MillerhCmd=0;
-  delete MillerkCmd; MillerkCmd=0;// New commands to support 4 Miller indice
-  delete MilleriCmd; MilleriCmd=0;
-  delete MillerlCmd; MillerlCmd=0;
+
 }
 
 
@@ -37,12 +31,5 @@ Caustic_PhononConfigMessenger::~Caustic_PhononConfigMessenger() {
 
 void Caustic_PhononConfigMessenger::SetNewValue(G4UIcommand* cmd, G4String value) {
   if (cmd == hitsCmd) theManager->SetHitOutput(value);
-  if (cmd == MillerhCmd)
-      theManager->SetMillerh(MillerhCmd->GetNewDoubleValue(value));
-  if (cmd == MillerkCmd)
-      theManager->SetMillerk(MillerkCmd->GetNewDoubleValue(value));
-  if (cmd == MilleriCmd)
-      theManager->SetMilleri(MilleriCmd->GetNewDoubleValue(value));
-if (cmd == MillerlCmd)
-          theManager->SetMillerl(MillerlCmd->GetNewDoubleValue(value));
+
 }
